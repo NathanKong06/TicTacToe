45df01dbfaff9b58cf0cb4d5c3445c85ca9b5433
NathanKong06 <81611522+NathanKong06@users.noreply.github.com>
2022-08-17 13:57:13 -0700

    Update Tic Tac Toe.cpp

--- src/Tic Tac Toe.cpp
+++ src/Tic Tac Toe.cpp
@@ -1,10 +1,3 @@
-//============================================================================
-// Name        : Tic.cpp
-// Author      : Nathan Kong
-// Version     :
-// Copyright   : Your copyright notice
-// Description : Hello World in C++, Ansi-style
-//============================================================================
 #include <iostream>
 #include <vector>
 
